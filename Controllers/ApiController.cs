a95395a9f944b632c4735216420043295ca51a98
HungPV <hungpv@goquo.com>
2020-06-04 20:40:00 +0700

    'on_dev'

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PaymentHandler.Controllers
@@ -16,18 +15,17 @@ namespace PaymentHandler.Controllers
         {
             return Ok($"{Request.Method} OK");
         }
-        
+
         [Route("notify_url")]
         [Consumes("application/x-www-form-urlencoded")]
-        public async Task<IActionResult> NotifyHandler([FromForm] IFormCollection value)
+        public async Task<IActionResult> NotifyHandler()
         {
             var sb = new StringBuilder();
-
             sb.AppendLine($"============ {DateTime.Now:HH:mm:ss} ============");
             sb.AppendLine(HttpContext.Connection.RemoteIpAddress.ToString());
 
             sb.AppendLine($"Method: {Request.Method} using {Request.Scheme}");
-            
+
             sb.AppendLine("******* Queries:");
             foreach (var q in Request.Query)
             {
@@ -41,7 +39,7 @@ namespace PaymentHandler.Controllers
             }
 
             sb.AppendLine();
-            
+
             sb.AppendLine("******* Headers:");
             foreach (var h in Request.Headers)
             {
@@ -55,40 +53,42 @@ namespace PaymentHandler.Controllers
             }
 
             sb.AppendLine();
-            
+
             var req = HttpContext.Request;
 
             // Allows using several time the stream in ASP.Net Core
 
-            sb.AppendLine("******* Body:");
-
-            try
-            {
-                using var reader 
-                    = new StreamReader(req.Body, Encoding.UTF8, true, 1024, true);
-                sb.AppendLine(await reader.ReadToEndAsync());
-            }
-            catch (Exception e)
-            {
-                Console.Error.WriteLine(e);
-                sb.AppendLine($"<read failure: {e.Message}>");
-            }
+            // sb.AppendLine("******* Body:");
+            //
+            // try
+            // {
+            //     using var reader
+            //         = new StreamReader(req.Body, Encoding.UTF8, true, 1024, true);
+            //     sb.AppendLine(await reader.ReadToEndAsync());
+            // }
+            // catch (Exception e)
+            // {
+            //     Console.Error.WriteLine(e);
+            //     sb.AppendLine($"<read failure: {e.Message}>");
+            // }
 
             sb.AppendLine("******* RAW Body:");
 
-            foreach (var vl in value)
-            {
-                sb.AppendLine("***");
-                sb.AppendLine(vl.Key);
-                sb.AppendLine("=>");
-                foreach (var v in vl.Value)
+            var values = Request.Form;
+            if (values?.Count > 0)
+                foreach (var vl in values)
                 {
-                    sb.AppendLine(v);
+                    sb.AppendLine("***");
+                    sb.AppendLine(vl.Key);
+                    sb.AppendLine("=>");
+                    foreach (var v in vl.Value)
+                    {
+                        sb.AppendLine(v);
+                    }
                 }
-            }
 
             Console.WriteLine(sb.ToString());
-            
+
             // Rewind, so the core is not lost when it looks the body for the request
             return Ok();
         }
